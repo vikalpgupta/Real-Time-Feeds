24fdae6d50f1ce1e3b1483a4452de485472cc4e0
vikalpgupta-birdeye <vikalp.gupta@birdeye.com>
2018-06-17 19:29:45 +0530

    Changing to Pure Stateless Component

--- client/src/components/TimeLine.js
+++ client/src/components/TimeLine.js
@@ -1,27 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Comments from "./Comments";
 
-class TimeLine extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            comment: false
-        };
-    }
+const TimeLine = () => {
+    const { timeline, sendPost } = this.props;
 
-    render() {
-        const { timeline, sendPost } = this.props;
-
-        return timeline && timeline.length ? <div>
-            {timeline.map(function (post) {
-                return (<div className="post" key={post.id}>
-                    <span className={"user-id"}>{post.userId}</span>
-                    <span className={"user-text"}>{post.text}</span>
-                    <Comments postId={post.id} comments={post.comments} sendPost={sendPost}></Comments>
-                </div>);
-            })}
-        </div> : null;
-    }
-}
+    return timeline && timeline.length ? <div>
+        {timeline.map(function (post) {
+            return (<div className="post" key={post.id}>
+                <span className={"user-id"}>{post.userId}</span>
+                <span className={"user-text"}>{post.text}</span>
+                <Comments postId={post.id} comments={post.comments} sendPost={sendPost}/>
+            </div>);
+        })}
+    </div> : null;
+};
 
 export default TimeLine;
